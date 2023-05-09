1f071fd9270af77f1d1929badbb811ea47e1ef6a
rafal <rafalgraczykowski737@gmail.com>
2023-05-09 13:38:44 +0200

    Add field to database

--- app/src/main/java/com/farm/field/AddFieldFragment.kt
+++ app/src/main/java/com/farm/field/AddFieldFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
 import com.farm.R
 
 
@@ -25,9 +27,19 @@ class AddFieldFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val arrayAdapter : ArrayAdapter<String> = ArrayAdapter(requireContext(),android.R.layout.simple_dropdown_item_1line, units)
-        area = view.findViewById(R.id.area)
+        area = view.findViewById(R.id.area_unit)
         area?.setAdapter(arrayAdapter)
         area?.setOnClickListener(showUnitList())
+        view.findViewById<Button>(R.id.add_field).setOnClickListener(addField())
+    }
+
+    private fun addField(): View.OnClickListener {
+        return View.OnClickListener {
+            val fieldName = view?.findViewById<EditText>(R.id.field_name)
+            val areaSize = view?.findViewById<EditText>(R.id.area_size)
+            val areaUnit = view?.findViewById<AutoCompleteTextView>(R.id.area_unit)
+
+        }
     }
 
     private fun showUnitList(): View.OnClickListener {
